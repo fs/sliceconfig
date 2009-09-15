8161b939425b172c1d56d7f37767be702b3e748a
Denis Klukvin <denis.klukvin@flatsoft.com>
2009-09-15 14:44:19 +0400

    - Update nginx to 0.6.36

--- install/scripts/install_nginx.sh
+++ install/scripts/install_nginx.sh
@@ -6,4 +6,4 @@
 . `dirname $0`/configuration.sh
 
 
-yum -y -d1 localinstall --nogpgcheck $sliceconfig/install/files/nginx-0.6.32-3.x86_64.rpm
+yum -y -d1 localinstall --nogpgcheck $sliceconfig/install/files/nginx-0.6.36-1.x86_64.rpm
