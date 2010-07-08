56f069b1ec9ca325f54de6c2add3a14a10c7a418
Denis Klykvin <denis.klukvin@flatsoft.com>
2010-07-08 14:54:42 +0400

    Fix passenger installation

--- install/scripts/install_ruby_passenger.sh
+++ install/scripts/install_ruby_passenger.sh
@@ -5,6 +5,6 @@
 # include config
 . `dirname $0`/configuration.sh
 
-echo gem install --ignore-dependencies --no-rdoc --no-ri --no-update-sources -q passenger --version=`get_value 'CONFIG_PASSENGER_VERSION'`
+gem install --ignore-dependencies --no-rdoc --no-ri --no-update-sources -q passenger --version=`get_value 'CONFIG_PASSENGER_VERSION'`
 passenger-install-apache2-module
 ln -s /opt/ruby-enterprise-1.8.7-2009.10/ /opt/ree
